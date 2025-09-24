5d9750ee4219f53ebdb5ce32c233cba620c71c67
Andrei Albu <albu.cosminandrei.1999@gmail.com>
2025-09-24 17:33:51 +0200

    feat

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ async function sendMessage() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer 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`
+        "Authorization": `Bearer ${import.meta.env.VITE_GROQ_API_KEY}`
         //"Authorization": `Bearer ${process.env.GROQ_API_KEY}`
       },
       body: JSON.stringify({
